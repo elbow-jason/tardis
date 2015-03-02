7cd8f31b3dac6295baaae96cd6b99fede94197aa
Jason Goldberger <jlgoldb2@asu.edu>
2015-03-02 14:47:42 -0700

    now function passes tests

--- lib/tardis.ex
+++ lib/tardis.ex
@@ -1,2 +1,9 @@
 defmodule Tardis do
+
+  def now do
+    {{year, month, day}, {hours, minutes, seconds}} = :calendar.universal_time()
+    {{year, month, day}, {hours, minutes, seconds}, "GMT"}
+  end
+
+
 end
--- test/tardis_test.exs
+++ test/tardis_test.exs
@@ -1,7 +1,14 @@
 defmodule TardisTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Tardis.now returns 3-item tuple with GMT descriptor" do
+    {{yr, mon, day}, {hr, min, sec}, "GMT"} = Tardis.now
+    assert is_integer(yr)
+    assert is_integer(mon)
+    assert is_integer(day)
+    assert is_integer(hr)
+    assert is_integer(min)
+    assert is_integer(sec)
   end
+
 end
